61b35e180d08c9e15b25504be747abf5c8e96cfc
Mayowa Ajiboye <mayaajiboye@gmail.com>
2023-01-30 17:45:58 -0500

    complete lab

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
-}
+  if(!props.isPublished){
+    return null
+  } else {
+      return (
+        <div>
+          <h1>{props.articleText}</h1>
+          <p>{props.minutesToRead}</p>
+        </div>
+      )
+
+
+  }
+
+  }
+  // console.log(props)
 
 export default BlogContent;
